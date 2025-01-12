6a3084a731e4709c7692506c6b60298b4cbfcda4
joshua <joshualessadias@gmail.com>
2025-01-12 14:26:28 -0300

    increasing size of the clickable point

--- lib/widgets/clickable_point.dart
+++ lib/widgets/clickable_point.dart
@@ -2,23 +2,17 @@ import 'package:flutter/material.dart';
 
 class ClickablePoint extends StatelessWidget {
   final int amount;
-  final VoidCallback onTap;
 
-  const ClickablePoint({Key? key, required this.amount, required this.onTap})
-      : super(key: key);
+  const ClickablePoint({Key? key, required this.amount}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      enableFeedback: true,
-      child: Text(
-        amount < 0 ? amount.toString() : "+$amount",
-        style: const TextStyle(
-          color: Colors.white,
-          decoration: TextDecoration.none,
-          fontSize: 20,
-        ),
+    return Text(
+      amount < 0 ? amount.toString() : "+$amount",
+      style: const TextStyle(
+        color: Colors.white,
+        decoration: TextDecoration.none,
+        fontSize: 20,
       ),
     );
   }
